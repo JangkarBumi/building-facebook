5c46d5286bc81db6ffab9d095e4b585f1b2a331d
Zeha Irawan <jangkaringbumi@gmail.com>
2019-04-26 21:31:22 +0700

    add comment test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # def setup
-  #   @comment = comments(:one)
-  # end
-  #
-  # test 'should valid?' do
-  #   assert @comment.valid?
-  # end
-  #
-  # test 'should not save post without body' do
-  #   comment = Comment.new
-  #   assert !comment.save
-  # end
+  def setup
+    @comment = comments(:one)
+  end
+  
+  test 'should valid?' do
+    assert @comment.valid?
+  end
+  
+  test 'should not save post without body' do
+    comment = Comment.new
+    assert !comment.save
+  end
 end
