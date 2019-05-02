96b8b0ad265d8a0cd2703d11c3c4f460fb4c716b
1ba1 <vvvalenti01@yahoo.it>
2019-05-02 16:04:16 +0200

    fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-   
-    def create
-        @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-        @friendship.save
-    end
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @friendship.save
+  end
 
-    def update
-    end
+  def update; end
 
-    def destroy
-        @friendship = Friendship.where(user_id:params[:id]).where(friend_id:current_user).first
+  def destroy
+    @friendship = Friendship.where(user_id: params[:id])
+                            .where(friend_id: current_user).first
     @friendship.destroy
-    end
+  end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,5 +5,5 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  enum status: [:pending, :confirmed]
+  enum status: %i[pending confirmed]
 end
