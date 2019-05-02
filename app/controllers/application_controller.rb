830529ab8222588491ecda19d9c84a3312d25862
1ba1 <vvvalenti01@yahoo.it>
2019-05-02 17:59:33 +0200

    add show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def require_login
+    redirect_to user_session_path unless current_user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :require_login, only: %i[show]
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
   def create
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
     @friendship.save
