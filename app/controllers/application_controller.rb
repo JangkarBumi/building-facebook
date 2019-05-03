75eea49040d95fca53df788f4c4536cd661ad7d7
Zeha Irawan <jangkaringbumi@gmail.com>
2019-05-03 16:43:25 +0700

    customize devise sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def require_login
     redirect_to user_session_path unless current_user
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :city ])
   end
 end
