ab7cee6addfb56f16115681f67723df25ad16feb
1ba1 <vvvalenti01@yahoo.it>
2019-05-06 15:53:36 +0200

    fix like functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,11 @@
 class LikesController < ApplicationController
-    
-    def create
-        like = current_user.likes.build(post_id: params[:id]).save
-        redirect_to posts_path
-    end
+  def create
+    @like = current_user.likes.build(post_id: params[:post_id]).save
+    redirect_to posts_path
+  end
 
-    def destroy
-        Like.where(post_id: params[:id]).find_by(user: current_user).destroy
-        redirect_to posts_path
-    end
+  def destroy
+    Like.where(post_id: params[:id]).find_by(user: current_user).destroy
+    redirect_to posts_path
+  end
 end
