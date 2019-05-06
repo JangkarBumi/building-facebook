137b99135e8fc52f7cb46eef12936ae5e7efa793
1ba1 <vvvalenti01@yahoo.it>
2019-05-06 12:12:12 +0200

    fix post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  def new; end
 
-  def new
-    @post = Post.new
-  end
-  
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   def create
-    @post = current_user.posts.build(params[:body])
-    if @post.save
-      redirect_to posts_path
-    else
-      redirect_to user_path(current_user)
-    end
+    @post = current_user.posts.build(post_params)
+    redirect_to posts_path if @post.save
   end
 
+  private
 
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
