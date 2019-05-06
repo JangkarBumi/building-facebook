bfbfb26de63e7428bc53dcd35c9f4c6588384461
Zeha Irawan <jangkaringbumi@gmail.com>
2019-05-06 16:42:46 +0700

    testing post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+  
   def index
     @posts = Post.all
   end
 
   def create
-    @post = Post.new(params[:body])
-    @post.save
+    @post = current_user.posts.build(params[:body])
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
-  def new
-  end
+
 end
